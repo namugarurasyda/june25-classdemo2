aa30fbb893aa80513c4ea65a33463e99a1c87e77
namugarurasyda <2100813912@std.kyu.ac.ug>
2025-08-03 15:43:56 +0300

    pylint control

--- greeting/tests.py
+++ greeting/tests.py
@@ -1,26 +1,38 @@
 """
 Tests for the greeting application.
+This module contains test cases for the 'greeting' Django app.
 """
+
 from django.test import TestCase, Client
-from django.urls import reverse 
+from django.urls import reverse
 
 
 class GreetingTests(TestCase):
-     """A test case for the greeting app."""
+    """A test case for the main greeting view."""
+
     def setUp(self):
-        """"Tests that the page returns a 200 status code."""
+        """Set up a test client for use in all test methods."""
         self.client = Client()
-        
-    def test_index_page(self): """"Tests that the page returns a 200 status code."""
-        self.client = Client()
-        response = self.client.get(reverse('index'))
+
+    def test_index_page(self):
+        """
+        Tests that the main index page returns a 200 status code,
+        contains the text 'Hello', and uses the correct template.
+        """
+        response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello')
-        self.assertTemplateUsed(response, 'index.html')
-        
-class GreetingFunctionalityTests(TestCase): 
-    """"Tests that the page returns a 200 status code."""
+        self.assertContains(response, "Hello")
+        self.assertTemplateUsed(response, "index.html")
+
+
+class GreetingFunctionalityTests(TestCase):
+    """A test case for the functionality of the greeting view."""
+
+    def setUp(self):
+        """Set up a test client."""
         self.client = Client()
+
     def test_greeting(self):
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.context['greeting'], 'Hello')
+        """Tests that the context contains the correct greeting."""
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.context["greeting"], "Hello")
